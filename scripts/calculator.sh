6ed52c00a0fc998959e05b097759b24e43196632
Karl Palmskog <palmskog@gmail.com>
2019-05-17 18:43:43 -0500

    update Makefile and ci, opam packages for programs

--- scripts/calculator.sh
+++ scripts/calculator.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if ! [ -x ./CalculatorMain.d.byte ]
+if ! [ -x ./CalculatorMain.native ]
 then
-    echo 'CalculatorMain.d.byte not found!'
-    echo 'Run `make CalculatorMain.d.byte` first.'
+    echo 'CalculatorMain.native not found!'
+    echo 'Run `make CalculatorMain.native` first.'
     exit 1
 fi
 
-(./CalculatorMain.d.byte -me 1 -mode server 1 127.0.0.1 9001 2 127.0.0.1 9002 3 127.0.0.1 9003 &) >server1.log 2>&1
+(./CalculatorMain.native -me 1 -mode server 1 127.0.0.1 9001 2 127.0.0.1 9002 3 127.0.0.1 9003 &) >server1.log 2>&1
 
-(./CalculatorMain.d.byte -me 3 -mode server 1 127.0.0.1 9001 2 127.0.0.1 9002 3 127.0.0.1 9003 &) >server3.log 2>&1
+(./CalculatorMain.native -me 3 -mode server 1 127.0.0.1 9001 2 127.0.0.1 9002 3 127.0.0.1 9003 &) >server3.log 2>&1
 
-./CalculatorMain.d.byte -me 2 -mode client -server 3 1 127.0.0.1 9001 2 127.0.0.1 9002 3 127.0.0.1 9003 | tee client.log
+./CalculatorMain.native -me 2 -mode client -server 3 1 127.0.0.1 9001 2 127.0.0.1 9002 3 127.0.0.1 9003 | tee client.log
--- scripts/tpc.sh
+++ scripts/tpc.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-if ! [ -x ./TPCMain.d.byte ]
+if ! [ -x ./TPCMain.native ]
 then
-    echo 'TPCMain.d.byte not found!'
-    echo 'Run `make TPCMain.d.byte` first.'
+    echo 'TPCMain.native not found!'
+    echo 'Run `make TPCMain.native` first.'
     exit 1
 fi
 
-(./TPCMain.d.byte -me 1 -mode participant 0 127.0.0.1 8000 1 127.0.0.1 8001 2 127.0.0.1 8002 3 127.0.0.1 8003 &) >particpant1.log
+(./TPCMain.native -me 1 -mode participant 0 127.0.0.1 8000 1 127.0.0.1 8001 2 127.0.0.1 8002 3 127.0.0.1 8003 &) >participant1.log
 
-(./TPCMain.d.byte -me 2 -mode participant 0 127.0.0.1 8000 1 127.0.0.1 8001 2 127.0.0.1 8002 3 127.0.0.1 8003 &) >particpant2.log
+(./TPCMain.native -me 2 -mode participant 0 127.0.0.1 8000 1 127.0.0.1 8001 2 127.0.0.1 8002 3 127.0.0.1 8003 &) >participant2.log
 
-(./TPCMain.d.byte -me 3 -mode participant 0 127.0.0.1 8000 1 127.0.0.1 8001 2 127.0.0.1 8002 3 127.0.0.1 8003 &) >particpant3.log
+(./TPCMain.native -me 3 -mode participant 0 127.0.0.1 8000 1 127.0.0.1 8001 2 127.0.0.1 8002 3 127.0.0.1 8003 &) >participant3.log
 
-./TPCMain.d.byte -me 0 -mode coordinator 0 127.0.0.1 8000 1 127.0.0.1 8001 2 127.0.0.1 8002 3 127.0.0.1 8003 | tee coordinator.log
+./TPCMain.native -me 0 -mode coordinator 0 127.0.0.1 8000 1 127.0.0.1 8001 2 127.0.0.1 8002 3 127.0.0.1 8003 | tee coordinator.log
